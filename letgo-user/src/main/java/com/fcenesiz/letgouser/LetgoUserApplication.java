502d27b3fdf6723b7c6e532259f21dd042ca9005
Ahmet Fatih Çenesiz <fcenesiz@gmail.com>
2022-11-18 12:07:23 +0300

    user moved to letgo-user module.

---
+++ letgo-user/src/main/java/com/fcenesiz/letgouser/LetgoUserApplication.java
@@ -0,0 +1,13 @@
+package com.fcenesiz.letgouser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetgoUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetgoUserApplication.class, args);
+    }
+
+}
---
+++ letgo-user/src/test/java/com/fcenesiz/letgouser/LetgoUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcenesiz.letgouser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetgoUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
